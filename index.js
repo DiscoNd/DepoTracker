bfe499f565d052e112c6b73b363a40de7224ec4b
Jimi <jimi.joggari@gmail.com>
2024-03-10 21:22:17 +0200

    hmmmm

--- index.js
+++ index.js
@@ -1,52 +1,72 @@
-let depoCount = 0;
-localStorage.setItem("key", JSON.stringify("value"));
-
-// Get saved data from localStorage
-localStorage.getItem("key") ? JSON.parse(localStorage.getItem("key")) : "";
-
-const depoCountFunc = () => {
-  depoCount++;
-  console.log(depoCount);
-  const depoCountElement = document.getElementById("depoCount");
-  depoCountElement.innerHTML = `depo count ${depoCount}`;
-};
+let profit = localStorage.getItem("profit")
+  ? JSON.parse(localStorage.getItem("profit"))
+  : 0;
+let depoCount = localStorage.getItem("depoCount")
+  ? JSON.parse(localStorage.getItem("depoCount"))
+  : 0;
 
 const resetDepoCount = () => {
+  localStorage.removeItem("depoCount");
   depoCount = 0;
-  const depoCountElement = document.getElementById("depoCount");
-  depoCountElement.innerHTML = `depo count ${depoCount}`;
+  document.getElementById("depoCount").innerHTML = `depo count ${depoCount}`;
 };
-function fetchData() {
-  fetch("data.json")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Data:", data);
-      // Do something with the fetched data here
-    })
-    .catch((error) => {
-      console.error("There was a problem fetching the data:", error);
-    });
-}
-// function downloadData() {
-//   const data = { message: "Hello, world!" };
-//   const jsonData = JSON.stringify(data);
+const resetProfit = () => {
+  localStorage.removeItem("profit");
+  profit = 0;
+  document.getElementById("profit").innerHTML = `profit ${profit}`;
+};
+const checkProfit = () => {
+  if (profit === 0) {
+    document.getElementById("profit").style.color = "black";
+  } else if (profit > 0) {
+    document.getElementById("profit").style.color = "green";
+  } else {
+    document.getElementById("profit").style.color = "red";
+  }
+};
+const onPageLoad = () => {
+  document.getElementById("profit").innerHTML = `profit ${profit} &#8364;`;
+  document.getElementById("depoCount").innerHTML = `depo count ${depoCount}`;
 
-//   const blob = new Blob([jsonData], { type: "application/json" });
-//   const url = URL.createObjectURL(blob);
+  checkProfit();
+};
 
-//   const a = document.createElement("a");
-//   a.href = url;
-//   a.download = "data.json";
-//   a.click();
+const add10Eur = () => {
+  localStorage.setItem("profit", JSON.stringify((profit -= 10)));
+  document.getElementById("profit").innerHTML = `profit ${profit} &#8364;`;
+  //depo count
+  localStorage.setItem("depoCount", JSON.stringify((depoCount += 1)));
+  document.getElementById("depoCount").innerHTML = `depo count ${depoCount}`;
+  checkProfit();
+};
+const add20Eur = () => {
+  localStorage.setItem("profit", JSON.stringify((profit -= 20)));
+  document.getElementById("profit").innerHTML = `profit ${profit} &#8364;`;
+  //depo count
+  localStorage.setItem("depoCount", JSON.stringify((depoCount += 1)));
+  document.getElementById("depoCount").innerHTML = `depo count ${depoCount}`;
+};
 
-//   // Clean up
-//   URL.revokeObjectURL(url);
-// }
-// downloadData();
+const add30Eur = () => {
+  localStorage.setItem("profit", JSON.stringify((profit -= 30)));
+  document.getElementById("profit").innerHTML = `profit ${profit} &#8364;`;
+  //depo count
+  localStorage.setItem("depoCount", JSON.stringify((depoCount += 1)));
+  document.getElementById("depoCount").innerHTML = `depo count ${depoCount}`;
+};
 
-fetchData();
+const add40Eur = () => {
+  localStorage.setItem("profit", JSON.stringify((profit -= 40)));
+  document.getElementById("profit").innerHTML = `profit ${profit} &#8364;`;
+  //depo count
+  localStorage.setItem("depoCount", JSON.stringify((depoCount += 1)));
+  document.getElementById("depoCount").innerHTML = `depo count ${depoCount}`;
+};
+
+const add50Eur = () => {
+  localStorage.setItem("profit", JSON.stringify((profit -= 50)));
+  document.getElementById("profit").innerHTML = `profit ${profit} &#8364;`;
+  //depo count
+  localStorage.setItem("depoCount", JSON.stringify((depoCount += 1)));
+  document.getElementById("depoCount").innerHTML = `depo count ${depoCount}`;
+};
